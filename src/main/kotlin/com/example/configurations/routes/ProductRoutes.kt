77304f4870789135c81b40a21b6f0fb631fe575b
Santiago Mattiauda <santimattius@gmail.com>
2022-03-09 22:22:48 -0300

    [quality] setub

--- src/main/kotlin/com/example/configurations/routes/ProductRoutes.kt
+++ src/main/kotlin/com/example/configurations/routes/ProductRoutes.kt
@@ -9,7 +9,11 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
-import io.ktor.server.routing.*
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.Routing
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.put
 
 
 fun Routing.productV1() {
